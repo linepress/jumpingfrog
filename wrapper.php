8e137eb11e39d288570c02cc931a7a6858e3dfcf
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2012-10-30 20:26:00 +0100

    default wrapper templatE

---
+++ wrapper.php
@@ -0,0 +1,11 @@
+<?php get_template_part( 'templates/header/head') ?>
+
+  <div class="main-container">
+    <div class="main wrapper clearfix">
+   
+    <?php include my_template_path(); ?>
+   
+    </div> <!-- #main -->
+  </div> <!-- #main-container -->
+
+<?php get_template_part( 'templates/footer');
