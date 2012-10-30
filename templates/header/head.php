551e8bede273f8124b35350e62fe3e5ebe872aad
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2012-10-30 20:21:01 +0100

    head template with better support for enqueueing styles, body styles

--- templates/header/head.php
+++ templates/header/head.php
@@ -5,13 +5,24 @@
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/modernizr-2.6.2.min.js"></script>
+  <?php
+    wp_enqueue_style( 'main', get_template_directory_uri() .'/assets/css/normalize.css' );
+    wp_enqueue_style( 'normalize', get_template_directory_uri() .'/assets/css/main.css' );
+  ?>
+  <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
 
   <?php wp_head(); ?>
 </head>
+
+<body <?php body_class(); ?>>
+
+  <!--[if lt IE 7]>
+      <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+  <![endif]-->
