439af39d12c56178938ad41024822b88d354940d
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2012-10-30 20:20:29 +0100

    footer template

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,9 @@
-<?php    
-    // Footer hook for javascript parsing
-    wp_footer(); ?>
-  </boby>
+    <div class="footer-container">
+      <footer class="wrapper">
+        <h3>footer</h3>
+      </footer>
+    </div> 
+  
+    <?php wp_footer(); // Footer hook for javascript parsing ?>
+  </body>
 </html>
