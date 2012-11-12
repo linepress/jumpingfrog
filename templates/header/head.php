6ae44272f3681e1cc271732c378ae2a2f649a09c
tim <tim@hoppinger.com>
2012-11-12 19:47:25 +0100

    Changed message of 'Your browser is ancient' to be visible if lt-ie8

--- templates/header/head.php
+++ templates/header/head.php
@@ -42,7 +42,7 @@
 <body <?php body_class(); ?>>
 
   <div id="wrap">
-  <!--[if lt IE 7]>
+  <!--[if lt IE 8]>
       <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
       <style type="text/css">
         #wrap {display:table;height:100%}
