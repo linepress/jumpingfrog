a481e69de92a3c0ee496c75bf89f706b30434e2e
sebastiaandegeus <sebastiaan@hoppinger.com>
2012-11-13 01:22:42 +0100

    prefixed some functions calls

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-get_main_template_part( 
+jf_get_main_template_part( 
   array(
   'templates/content/page'
   )
 );
 
-get_sidebar_template_part( 
+jf_get_sidebar_template_part( 
   array(
   'templates/sidebar/example',
   'templates/sidebar/another-example',
