ccaf345b76e1b23fb6108881453559a1d1381a45
sebastiaandegeus <sebastiaan@hoppinger.com>
2012-11-13 01:24:18 +0100

    removed unused extra style.cs and moved jquery to footer

--- templates/header/head.php
+++ templates/header/head.php
@@ -2,14 +2,13 @@
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes() ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title><?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <link type="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css'" />
+  <title><?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?></title>
 
   <?php
     if( ENVIRONMENT == 'prod' ) { 
@@ -19,9 +18,6 @@
     }
   ?>
   <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
 
   <?php wp_head(); ?>
 
