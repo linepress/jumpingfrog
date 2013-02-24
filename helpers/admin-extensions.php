4e5ab35336e9afa6b81a55754158986874c36514
timhuisman <tim@worksoftime.nl>
2013-02-24 15:06:50 +0100

    Corrected function names

--- helpers/admin-extensions.php
+++ helpers/admin-extensions.php
@@ -1,12 +1,12 @@
 <?php 
 
-function jumpingfrog_load_custom_wp_admin_script( $hook ) {
-  wp_register_script( '{context}', get_template_directory_uri() . '/assets/js/admin.js', array( 'jquery' ), null, true );
-  wp_enqueue_script( '{context}_admin' );
+function jf_load_custom_wp_admin_script( $hook ) {
+  wp_register_script( 'admin_js', get_template_directory_uri() . '/assets/js/admin.js', array( 'jquery' ), null, true );
+  wp_enqueue_script( 'admin_js' );
 }
 
-function jumpingfrog_load_custom_wp_admin_style() {
-  wp_register_style( 'admin_css', get_bloginfo( 'stylesheet_directory' ) . '/assets/css/admin.css', false, '1.0.0' );
+function jf_load_custom_wp_admin_style() {
+  wp_register_style( 'admin_css', get_template_directory_uri() . '/assets/css/admin.css', false, '1.0.0' );
   wp_enqueue_style( 'admin_css' );
 }
 
@@ -15,5 +15,5 @@ function jumpingfrog_load_custom_wp_admin_style() {
  * The css and js files are located in the assets folder.
  * Replace all {context} occurances with your theme context.
  */
-// add_action( 'admin_enqueue_scripts', 'kwa_load_custom_wp_admin_style' );
-// add_action( 'admin_enqueue_scripts', 'jumpingfrog_load_custom_wp_admin_script' );
+// add_action( 'admin_enqueue_scripts', 'jf_load_custom_wp_admin_style' );
+// add_action( 'admin_enqueue_scripts', 'jf_load_custom_wp_admin_script' );
