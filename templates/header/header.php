e455a355fbc7a0a82315485e1b206454378b46b8
timhuisman <tim@worksoftime.nl>
2013-02-24 15:15:16 +0100

    Added seperate main menu template + custom walker class in functions.php | closes #40

--- templates/header/header.php
+++ templates/header/header.php
@@ -4,27 +4,8 @@
       <h1 class="hide"><?php echo bloginfo( 'name' ); ?></h1>
       <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="<?php echo bloginfo( 'name' ); ?>" title="Terug naar homepage" />
     </a>
-    <nav class="nav-main" role="navigation">
-      <?php 
-        wp_nav_menu ( array(
-          'theme_location'  => '',
-          'menu'            => 'main', 
-          'container'       => 'div', 
-          'container_class' => 'menu-{menu slug}-container', 
-          'container_id'    => '',
-          'menu_class'      => 'menu', 
-          'menu_id'         => '',
-          'echo'            => true,
-          'fallback_cb'     => 'wp_page_menu',
-          'before'          => '',
-          'after'           => '',
-          'link_before'     => '',
-          'link_after'      => '',
-          'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-          'depth'           => 0,
-          'walker'          => ''
-        ) );
-      ?>
-    </nav>
+
+    <?php get_template_part( 'templates/header/main-menu' ); ?>
+
   </div>  <!--- .header > .container -->
 </header> <!--- .header -->
