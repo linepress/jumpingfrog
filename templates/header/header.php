ac9519dc164ff17bc3df55e45674215d235cb24a
timhuisman <tim@worksoftime.nl>
2013-03-09 12:27:18 +0100

    Corrected use of <h1> on logo: no h1 if single/page - h1 with archive name/site name on archives and frontpage | closes #48

--- templates/header/header.php
+++ templates/header/header.php
@@ -1,11 +1,28 @@
+<?php
+  $logo_url = get_template_directory_uri() . '/assets/images/logo.png';
+?>
+
 <header class="header" role="banner">
   <div class="container">
-    <a class="title" href="/">
-      <h1 class="hide"><?php echo bloginfo( 'name' ); ?></h1>
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="<?php echo bloginfo( 'name' ); ?>" title="Terug naar homepage" />
-    </a>
+    <?php if( is_front_page() ): ?>
+      <h1>
+        <a class="title" href="/">
+          <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+        </a>
+      </h1>
+    <?php elseif( is_post_type_archive() ): ?>
+      <h1>
+        <a class="title" href="/">
+          <img src="<?php echo $logo_url; ?>" alt="<?php post_type_archive_title(); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+        </a>
+      </h1>
+    <?php else: ?>
+       <a class="title" href="/">
+        <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+      </a>
+    <?php endif; ?>
 
     <?php get_template_part( 'templates/header/main-menu' ); ?>
 
-  </div>  <!--- .header > .container -->
+  </div> <!--- .header > .container -->
 </header> <!--- .header -->
