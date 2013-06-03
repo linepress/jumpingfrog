63a8b9aa751f2d988673ce6c5f2149a628e3b882
Sebastiaan <sebastiaan@hoppinger.com>
2013-06-03 09:42:30 +0300

    Update head.php
    By using wp_title & blog_info name the site title got shown twice.

--- templates/header/head.php
+++ templates/header/head.php
@@ -8,7 +8,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <title><?php wp_title('|', true, 'right'); bloginfo( 'name' ); ?></title>
+  <title><?php wp_title('|', true, 'right') ?></title>
 
   <?php
     if( ENVIRONMENT == 'prod' ) { 
