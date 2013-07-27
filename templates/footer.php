7658850cbda9e1b068bf0553753b0468e0c208e0
timhuisman <tim@worksoftime.nl>
2013-07-27 23:39:36 +0200

    Emptied main & plugins + updated jQuery to 1.10.2, Modernizr to 2.6.2 and Respond to 1.2.0

--- templates/footer.php
+++ templates/footer.php
@@ -6,9 +6,6 @@
       </div> <!--- .footer > .container -->
     </footer> <!--- .footer -->
     
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
-
     <?php wp_footer(); // Footer hook for javascript parsing ?>
   </body>
 </html>
