f2510932ede466560e1ba32d52e243bba11686fe
timhuisman <tim@worksoftime.nl>
2013-09-22 12:48:30 +0200

    Fixed title attr value on the logo | Closes #57

--- templates/header/header.php
+++ templates/header/header.php
@@ -7,18 +7,18 @@
     <?php if( is_front_page() ): ?>
       <h1>
         <a class="title" href="/">
-          <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+          <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php echo sprintf( __( 'Return to the homepage of %1s', 'jumpingfrog' ), get_bloginfo( 'name' ) ); ?>" />
         </a>
       </h1>
     <?php elseif( is_post_type_archive() ): ?>
       <h1>
         <a class="title" href="/">
-          <img src="<?php echo $logo_url; ?>" alt="<?php post_type_archive_title(); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+          <img src="<?php echo $logo_url; ?>" alt="<?php post_type_archive_title(); ?>" title="<?php echo sprintf( __( 'Return to the homepage of %1s', 'jumpingfrog' ), get_bloginfo( 'name' ) ); ?>" />
         </a>
       </h1>
     <?php else: ?>
        <a class="title" href="/">
-        <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php __('Return to homepage', 'jumpingfrog'); ?>" />
+        <img src="<?php echo $logo_url; ?>" alt="<?php echo bloginfo( 'name' ); ?>" title="<?php echo sprintf( __( 'Return to the homepage of %1s', 'jumpingfrog' ), get_bloginfo( 'name' ) ); ?>" />
       </a>
     <?php endif; ?>
 
