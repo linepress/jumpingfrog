86e47097574d3b02ba33476e77bfe9cd74cac9cb
timhuisman <tim@worksoftime.nl>
2013-09-22 12:42:49 +0200

    Removed Google Frame (is being retired) code

--- templates/header/head.php
+++ templates/header/head.php
@@ -37,9 +37,3 @@
 <body <?php body_class(); ?>>
 
   <div class="wrap" role="document">
-  <!--[if lt IE 8]>
-      <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-      <style type="text/css">
-        .wrap {display:table;height:100%}
-      </style>
-  <![endif]-->
