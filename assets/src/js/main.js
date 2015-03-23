761b4409ca4f2915265cc4f469bd3a5c78e31a23
Tim Huisman <tim@hoppinger.com>
2015-03-23 11:59:07 +0100

    Moved helper function to seperate file + added basic global event listeners in main to init

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,32 +1,31 @@
 // ==========================================================================
-// Include own script here
+// Init Scripts
 // ==========================================================================
 
 ;(function ($) {
+
+  // ==========================================================================
+  // Execute functions on document.ready
+  // ==========================================================================
 
   $( document ).ready( function() {
 
   });
 
+  // ==========================================================================
+  // Execute functions on window.load
+  // ==========================================================================
+
+  $( window ).load( function() {
+
+  });
+
+  // ==========================================================================
+  // Execute functions on window.resize
+  // ==========================================================================
+
+  $( window ).resize( function() {
+
+  });
+
 })(jQuery);
-
-// ==========================================================================
-// Function returns an array with viewport width and height, that can be used
-// to activate JS on certain CSS viewports. How to use: get_viewport_size().height
-//
-// $(document).width(), $(body).width can return different values between
-// browsers, this function always returns the same value
-// ==========================================================================
-
-get_viewport_size = function() {
-  var e = window, a = 'inner';
-  if (!('innerWidth' in window )) {
-    a = 'client';
-    e = document.documentElement || document.body;
-  }
-  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
-};
-
-// ==========================================================================
-// Include plugins here (the minified version as much as possible)
-// ==========================================================================
